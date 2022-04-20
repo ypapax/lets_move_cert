bce2f355be580cea253d261a6307e09e6df52eeb
Maxim Yefremov <maxYefr@gmail.com>
2022-04-20 06:50:27 +0300

    send live folder too

--- move.sh
+++ move.sh
@@ -4,14 +4,14 @@ d=$1 # domain like sub.someDomain.com
 t=$2 # target server like user@ip
 
 remoteFolder=/
-sudo tar -chvzf certs.tar.gz /etc/letsencrypt/archive/$d /etc/letsencrypt/renewal/$d.conf
+sudo tar -chvzf certs.tar.gz /etc/letsencrypt/live/$d /etc/letsencrypt/archive/$d /etc/letsencrypt/renewal/$d.conf
 scp certs.tar.gz $t:$remoteFolder
 
 ssh $t "cd $remoteFolder; tar -xvf certs.tar.gz"
 
-ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/cert2.pem /etc/letsencrypt/live/$d/cert.pem"
-ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/chain2.pem /etc/letsencrypt/live/$d/chain.pem"
-ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/fullchain2.pem /etc/letsencrypt/live/$d/fullchain.pem"
-ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/privkey2.pem /etc/letsencrypt/live/$d/privkey.pem"
+#ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/cert2.pem /etc/letsencrypt/live/$d/cert.pem"
+#ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/chain2.pem /etc/letsencrypt/live/$d/chain.pem"
+#ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/fullchain2.pem /etc/letsencrypt/live/$d/fullchain.pem"
+#ssh $t "sudo ln -sf /etc/letsencrypt/archive/$d/privkey2.pem /etc/letsencrypt/live/$d/privkey.pem"
 
 
